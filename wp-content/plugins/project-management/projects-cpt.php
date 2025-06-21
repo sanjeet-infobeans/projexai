df1d11f8dce8ccb0d0f653697d5de086c02ee255
rahul-gpm <rnagar@goldenpeakmedia.com>
2025-06-21 18:25:13 +0530

    Added client requirement field in project CPT

--- wp-content/plugins/project-management/projects-cpt.php
+++ wp-content/plugins/project-management/projects-cpt.php
@@ -101,4 +101,55 @@ add_action('save_post_projects', function($post_id) {
     }
 });
 
+// Add meta box for Client Requirement (text + attachment)
+add_action('add_meta_boxes', function() {
+    add_meta_box(
+        'project_client_requirement',
+        'Client Requirement',
+        function($post) {
+            $requirement = get_post_meta($post->ID, '_project_client_requirement', true);
+            $attachment_id = get_post_meta($post->ID, '_project_client_requirement_attachment', true);
+            $attachment_url = $attachment_id ? wp_get_attachment_url($attachment_id) : '';
+            echo '<label for="project_client_requirement">Requirement:</label>';
+            echo '<textarea name="project_client_requirement" id="project_client_requirement" rows="4" style="width:100%">' . esc_textarea($requirement) . '</textarea>';
+            echo '<br><br>';
+            echo '<label for="project_client_requirement_attachment">Attachment:</label><br>';
+            if ($attachment_url) {
+                echo '<a href="' . esc_url($attachment_url) . '" target="_blank">View Current Attachment</a><br>';
+            }
+            echo '<input type="file" name="project_client_requirement_attachment" id="project_client_requirement_attachment" />';
+        },
+        'projects',
+        'normal',
+        'default'
+    );
+});
+
+add_action('save_post_projects', function($post_id) {
+    // Save requirement text
+    if (isset($_POST['project_client_requirement'])) {
+        update_post_meta($post_id, '_project_client_requirement', sanitize_textarea_field($_POST['project_client_requirement']));
+    }
+    // Save attachment
+    if (!empty($_FILES['project_client_requirement_attachment']['name'])) {
+        require_once(ABSPATH . 'wp-admin/includes/file.php');
+        $file = $_FILES['project_client_requirement_attachment'];
+        $upload = wp_handle_upload($file, ['test_form' => false]);
+        if (!isset($upload['error']) && isset($upload['file'])) {
+            $filetype = wp_check_filetype(basename($upload['file']), null);
+            $attachment = [
+                'post_mime_type' => $filetype['type'],
+                'post_title' => sanitize_file_name($file['name']),
+                'post_content' => '',
+                'post_status' => 'inherit'
+            ];
+            $attach_id = wp_insert_attachment($attachment, $upload['file'], $post_id);
+            require_once(ABSPATH . 'wp-admin/includes/image.php');
+            $attach_data = wp_generate_attachment_metadata($attach_id, $upload['file']);
+            wp_update_attachment_metadata($attach_id, $attach_data);
+            update_post_meta($post_id, '_project_client_requirement_attachment', $attach_id);
+        }
+    }
+});
+
 // Tech Stake taxonomy assumed to be registered elsewhere and will appear in the sidebar by default. 
